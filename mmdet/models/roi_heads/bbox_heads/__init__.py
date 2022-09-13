7a12f03b784e5797521b0aaf1a1deeb12b86b2f2
EdSong <46233799+mousecpn@users.noreply.github.com>
2022-09-13 21:45:03 +0800

    Update __init__.py

--- mmdet/models/roi_heads/bbox_heads/__init__.py
+++ mmdet/models/roi_heads/bbox_heads/__init__.py
@@ -4,7 +4,7 @@ from .convfc_bbox_head import (ConvFCBBoxHead, Shared2FCBBoxHead,
                                Shared4Conv1FCBBoxHead, ProbShared2FCBBoxHead, ProbConvFCBBoxHead)
 from .dii_head import DIIHead
 from .double_bbox_head import DoubleConvFCBBoxHead
-from .sabl_head import SABLHead,ProbSABLHead
+from .sabl_head import SABLHead
 from .scnet_bbox_head import SCNetBBoxHead
 __all__ = [
     'BBoxHead', 'ConvFCBBoxHead', 'Shared2FCBBoxHead',
