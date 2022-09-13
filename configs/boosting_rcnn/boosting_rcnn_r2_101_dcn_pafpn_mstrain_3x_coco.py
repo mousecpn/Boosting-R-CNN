06512b6c3a3828a3714310d41c568b7b38131e17
EdSong <46233799+mousecpn@users.noreply.github.com>
2022-09-13 22:20:49 +0800

    Update boosting_rcnn_r2_101_dcn_pafpn_mstrain_3x_coco.py

--- configs/boosting_rcnn/boosting_rcnn_r2_101_dcn_pafpn_mstrain_3x_coco.py
+++ configs/boosting_rcnn/boosting_rcnn_r2_101_dcn_pafpn_mstrain_3x_coco.py
@@ -34,7 +34,7 @@ model = dict(
 
 
 dataset_type = 'CocoDataset'
-data_root = '/data/Datasets/dailh/coco/'
+data_root = 'data/coco/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
